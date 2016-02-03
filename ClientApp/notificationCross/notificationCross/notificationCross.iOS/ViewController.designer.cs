9a2a7c41176ad642f8feb7aa6fce46aef32588ee
szymid <szymid@interia.pl>
2016-02-03 01:08:46 +0100

    iOS UI First Part

--- ClientApp/notificationCross/notificationCross/notificationCross.iOS/ViewController.designer.cs
+++ ClientApp/notificationCross/notificationCross/notificationCross.iOS/ViewController.designer.cs
@@ -1,17 +1,61 @@
-﻿//
-// This file has been generated automatically by MonoDevelop to store outlets and
-// actions made in the Xcode designer. If it is removed, they will be lost.
-// Manual changes to this file may not be handled correctly.
+// WARNING
+//
+// This file has been generated automatically by Xamarin Studio from the outlets and
+// actions declared in your storyboard file.
+// Manual changes to this file will not be maintained.
 //
 using Foundation;
+using System;
+using System.CodeDom.Compiler;
+using UIKit;
 
 namespace notificationCross.iOS
 {
 	[Register ("ViewController")]
 	partial class ViewController
 	{
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UIButton notificationButton1 { get; set; }
+
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UIButton notificationButton2 { get; set; }
+
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UIButton notificationButton3 { get; set; }
+
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UIButton notificationButton4 { get; set; }
+
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UIButton notificationButton5 { get; set; }
+
 		void ReleaseDesignerOutlets ()
 		{
+			if (notificationButton1 != null) {
+				notificationButton1.Dispose ();
+				notificationButton1 = null;
+			}
+			if (notificationButton2 != null) {
+				notificationButton2.Dispose ();
+				notificationButton2 = null;
+			}
+			if (notificationButton3 != null) {
+				notificationButton3.Dispose ();
+				notificationButton3 = null;
+			}
+			if (notificationButton4 != null) {
+				notificationButton4.Dispose ();
+				notificationButton4 = null;
+			}
+			if (notificationButton5 != null) {
+				notificationButton5.Dispose ();
+				notificationButton5 = null;
+			}
 		}
 	}
 }
